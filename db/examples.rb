a2a7e9c84edacb9e49365ed4387b8c178c611956
yourUsername <your_email@example.com>
2017-02-17 19:08:30 -0500

    Database examples updated

--- db/examples.rb
+++ db/examples.rb
@@ -17,3 +17,29 @@
 #                password: 'abc123',
 #                password_confirmation: nil)
 # end
+# frozen_string_literal: true
+# This file should contain all the record creation needed to experiment with
+# your app during development.
+#
+# The data can then be loaded with the rake db:examples (or created alongside
+# the db with db:nuke_pave).
+#
+# Examples:
+#
+#   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
+#   Mayor.create(name: 'Emanuel', city: cities.first)
+
+%w(alice bob charlie dana eliot franky gloria henry iulia).each do |name|
+  email = "#{name}@#{name}.com"
+  next if User.exists? email: email
+  User.create!(email: email,
+               password: 'abc123',
+               password_confirmation: nil)
+
+  Student.create!(first_name: name.to_s,
+                  last_name: 'Wisner',
+                  born_on: '2010-01-02',
+                  school: 'Bronxville',
+                  teacher: 'Gillin',
+                  grade: '4')
+end
