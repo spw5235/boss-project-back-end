2d3bf173e0e3e5c71dfce612c4bd018417de48d0
yourUsername <your_email@example.com>
2017-02-21 16:01:13 -0500

    Observation Update and Destroy Scripts

--- scripts/observations/update.sh
+++ scripts/observations/update.sh
@@ -5,7 +5,7 @@ URL_PATH="/observations/${ID}"
 curl "${API}${URL_PATH}" \
   --include \
   --request PATCH \
-  # --header "Authorization: Token token=${TOKEN}" \
+  --header "Authorization: Token token=${TOKEN}" \
   --header "Content-Type: application/json" \
   --data '{
     "observation": {
