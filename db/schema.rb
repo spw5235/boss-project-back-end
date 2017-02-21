7ee55400d87eca385acbcb672d210391da72d7d0
yourUsername <your_email@example.com>
2017-02-21 14:57:41 -0500

    Add User ID to Observations Migration

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170220233643) do
+ActiveRecord::Schema.define(version: 20170221195217) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -38,7 +38,9 @@ ActiveRecord::Schema.define(version: 20170220233643) do
     t.datetime "created_at",  null: false
     t.datetime "updated_at",  null: false
     t.integer  "student_id"
+    t.integer  "user_id"
     t.index ["student_id"], name: "index_observations_on_student_id", using: :btree
+    t.index ["user_id"], name: "index_observations_on_user_id", using: :btree
   end
 
   create_table "students", force: :cascade do |t|
@@ -66,5 +68,6 @@ ActiveRecord::Schema.define(version: 20170220233643) do
 
   add_foreign_key "examples", "users"
   add_foreign_key "observations", "students"
+  add_foreign_key "observations", "users"
   add_foreign_key "students", "users"
 end
